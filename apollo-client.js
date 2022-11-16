6c5711f74934d67efba50aa1aab7099b09225271
carlos815 <carlos_t815@outlook.com>
2022-11-16 18:08:47 -0500

    addesd ssr support via apollo

--- apollo-client.js
+++ apollo-client.js
@@ -3,6 +3,7 @@
 import { ApolloClient, InMemoryCache } from "@apollo/client";
 
 const client = new ApolloClient({
+    ssrMode: true,
     uri: "http://localhost:3000/api/graphql",
     cache: new InMemoryCache(),
 });
