ff9177a6a09acad96f3874c75a08b98e18074437
carlos815 <carlos_t815@outlook.com>
2022-11-16 11:42:55 -0500

    global page styles

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,6 +2,7 @@ import { ApolloProvider } from "@apollo/client";
 import client from "../apollo-client";
 import Header from "../components/Header"
 import Footer from "../components/Footer"
+import Page from "../components/Page"
 
 
 import "../styles/globals.css";
@@ -10,8 +11,10 @@ function MyApp({ Component, pageProps }) {
   return (
     <ApolloProvider client={client}>
       <Header />
-      <Component {...pageProps} />
-      <Footer/>
+      <Page>
+        <Component {...pageProps} />
+      </Page>
+      <Footer />
     </ApolloProvider>
   );
 }
