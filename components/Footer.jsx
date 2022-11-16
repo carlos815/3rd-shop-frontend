0e9d8fdc1a3bbac94f99c774093a66022737ccbc
carlos815 <carlos_t815@outlook.com>
2022-11-16 16:22:39 -0500

    implemented footer

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -1,3 +1,9 @@
+import Link from "next/link";
+
 export default function Header() {
-    return <div>This is the footer</div>
+    return <div className="flex gap-4 justify-center p-4 text-yellow underline bg-purple">
+        <Link href="/contact">Contact</Link>
+        <Link href="/legal">Legal Stuff</Link>
+        <Link href="/about">About Us</Link>
+    </div>
 }
