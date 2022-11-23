2fc8ff9055dbc0f01e1de9d0fbcc6b22f2473925
carlos815 <carlos_t815@outlook.com>
2022-11-23 10:52:03 -0500

    added nprogress, navstateprovider and styles

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,22 +1,47 @@
-import { ApolloProvider } from "@apollo/client";
+import { ApolloProvider, } from "@apollo/client";
 import client from "../apollo-client";
 import Header from "../components/Header"
 import Footer from "../components/Footer"
 import Page from "../components/Page"
+import { NavStateProvider } from "../lib/navStateProvider"
+import NProgress from 'nprogress';
+// import withData from '../lib/withData';
+
+
 
 
 import "../styles/globals.css";
+import { Router } from "next/router";
+
+Router.events.on('routeChangeStart', () => NProgress.start());
+Router.events.on('routeChangeComplete', () => NProgress.done());
+Router.events.on('routeChangeError', () => NProgress.done());
 
 function MyApp({ Component, pageProps }) {
   return (
-    <ApolloProvider client={client}>
-      <Header />
-      <Page>
-        <Component {...pageProps} />
-      </Page>
-      <Footer />
-    </ApolloProvider>
+    <div className="min-h-screen bg-purple flex flex-col justify-between">
+      <ApolloProvider client={client}>
+        <NavStateProvider>
+          <Header />
+          <Page>
+            <Component {...pageProps} />
+          </Page>
+          <Footer />
+        </NavStateProvider>
+      </ApolloProvider>
+    </div>
   );
 }
+
+// MyApp.getInitialProps = async function ({ Component, ctx }) {
+//   let pageProps = {};
+//   if (Component.getInitialProps) {
+//     pageProps = await Component.getInitialProps(ctx);
+//   }
+//   pageProps.query = ctx.query;
+//   return { pageProps };
+// };
+
+
 
 export default MyApp;
