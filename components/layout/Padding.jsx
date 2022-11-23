9320d03b95fae6a5825f4952cde96acc0ef7aeb0
carlos815 <carlos_t815@outlook.com>
2022-11-23 10:49:32 -0500

    adjusted styles

--- components/layout/Padding.jsx
+++ components/layout/Padding.jsx
@@ -2,7 +2,7 @@ export default function Padding({ children, ...props }) {
 
     const { className } = props
 
-    return <div className={`p-6 xl:px-0 mb-6 ${className}`}>
+    return <div className={`px-6 xl:px-0  ${className}`}>
         {children}
     </div>
 }
