a3b25438afc2dedd9b772f867aacce98868b9e4d
carlos815 <carlos_t815@outlook.com>
2022-11-23 10:52:17 -0500

    user utility to check logged in state

---
+++ components/User.js
@@ -0,0 +1,37 @@
+import { gql, useQuery } from '@apollo/client';
+
+const CURRENT_USER_QUERY = gql`
+  query {
+    authenticatedItem {
+      ... on User {
+        id
+        email
+        name    
+      }
+    }
+  }
+`;
+
+//     cart {
+//   id
+//   quantity
+//           product {
+//     id
+//     price
+//     name
+//     description
+//             photo {
+//               image {
+//         publicUrlTransformed
+//       }
+//     }
+//   }
+// }
+
+export function useUser() {
+  // useQuery(CURRENT_USER_QUERY)
+  const { data } = useQuery(CURRENT_USER_QUERY);
+  return data?.authenticatedItem;
+}
+
+export { CURRENT_USER_QUERY };
