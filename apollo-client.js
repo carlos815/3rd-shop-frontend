9e1c11a12df7335ef0732d5c1bb589440ab13c33
carlos815 <carlos_t815@outlook.com>
2022-11-30 13:06:24 -0500

    updated production server name

--- apollo-client.js
+++ apollo-client.js
@@ -2,7 +2,7 @@ import { ApolloClient, createHttpLink, InMemoryCache } from '@apollo/client';
 import { setContext } from '@apollo/client/link/context';
 
 
-const uri = process.env.NODE_ENV == "production" ? "http://localhost:6969/api/graphql" : "http://localhost:3000/api/graphql"
+const uri = process.env.NODE_ENV == "production" ? "https://backend-3rd-shop.herokuapp.com/api/graphql" : "http://localhost:3000/api/graphql"
 
 const link = createHttpLink({
     uri: uri,
