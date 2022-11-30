413ec1bdfb0dcc923d375d4217a99c87a8fca0f5
carlos815 <carlos_t815@outlook.com>
2022-11-30 11:25:12 -0500

    adjusted server settings

--- apollo-client.js
+++ apollo-client.js
@@ -1,8 +1,11 @@
 import { ApolloClient, createHttpLink, InMemoryCache } from '@apollo/client';
 import { setContext } from '@apollo/client/link/context';
 
+
+const uri = process.env.NODE_ENV == "production" ? "http://localhost:6969/api/graphql" : "http://localhost:3000/api/graphql"
+
 const link = createHttpLink({
-    uri: "http://localhost:3000/api/graphql",
+    uri: uri,
     credentials: 'include'
 });
 
