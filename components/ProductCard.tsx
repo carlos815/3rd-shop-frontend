16e2e8c56f8ad64aedfe961b770b93d0b9c90ece
carlos815 <carlos_t815@outlook.com>
2022-11-30 09:58:56 -0500

    adjusted styles product Card

--- components/ProductCard.tsx
+++ components/ProductCard.tsx
@@ -9,15 +9,15 @@ const ProductCard = ({ product }) => {
   const { altText, image } = product?.photo[0];
   return (
     <Link href={`/product/${id}`} className="group ">
-      <div className="w-full bg-gray-200 rounded-3xl overflow-hidden drop-shadow-lg ">
-        <div className="relative  h-64 group-hover:scale-105 group-hover:rotate-3 duration-500 ">
-          <Image
-            src={image.publicUrlTransformed}
-            alt={altText}
-            layout="fill"
-            objectFit="cover"
-          />
-        </div>
+      <div className="w-full bg-gray-200 rounded-lg overflow-hidden drop-shadow-lg ">
+        <Image
+          src={image.publicUrlTransformed}
+          alt={altText}
+          width={296}
+          height={256}
+          className="object-cover w-full relative  aspect-[296/256]  group-hover:scale-105 group-hover:rotate-3 duration-500 "
+        />
+
       </div>
       <h3 className="mt-4 text-turquoise font-headline text-lg ">{name}</h3>
       <p className="mt-0 text-pink font-headline text-lg">{formatMoney(price)}</p>
