d40a9de488b657cdb548bd4d9d15feffc3883923
carlos815 <carlos_t815@outlook.com>
2022-11-30 20:15:14 -0500

    styles adjusted

--- components/NavUrl.jsx
+++ components/NavUrl.jsx
@@ -1,7 +1,7 @@
 import Link from "next/link"
 
-const NavUrl = ({ href, children, onClick }) => <Link href={href} className="hover:even:rotate-6 hover:odd:-rotate-6  hover:z-40 group " onClick={onClick} >
-    <li className="z- p-2 font-bold before:rounded-xl  duration-100  font-headline  justify-center before:block before:absolute before:-inset-1  group-hover:before:bg-turquoise before:drop-shadow-lg before:-z-10 relative inline-block  group-hover:scale-125 ">
+const NavUrl = ({ href, children, onClick }) => <Link href={href} className="*hover:even:rotate-6 *hover:odd:-rotate-6  hover:z-40 group " onClick={onClick} >
+    <li className="z- p-2 font-bold before:rounded-xl  duration-100  font-headline  justify-center before:block before:absolute before:-inset-1  group-hover:before:bg-turquoise before:drop-shadow-lg before:-z-10 relative inline-block  group-hover:scale-150  ">
         {children}</li>
 </Link>
 
